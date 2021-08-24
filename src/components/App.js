d69a9c5b9b83869cc7eda376f40a11ac4031be1d
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2021-08-24 20:02:22 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import { AuthProvider } from "../contexts/AuthContext";
 
 import Chats from "./Chats";
