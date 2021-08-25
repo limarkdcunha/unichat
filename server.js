50104b977ba43475b2c90190f1c9e968ed707447
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2021-08-25 21:23:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,4 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, 'build');
-
-// serve the build folder
-app.use(express.static(publicPath));
-
-// In case, user requests a resource not in build/, ensure that index.html is served
-app.get('*', (req, res, next) => {
-  if (
-    req.headers['x-forward-proto'] !== 'https' &&
-    process.env.NODE_ENV === 'production'
-  ) {
-    res.redirect('https://' + req.headers.host + req.url);
-    res.sendFile(path.join(publicPath, 'index.html'));
-  } else {
-    next();
-  }
-});
-
-// start the server on given port (local env: port 3000)
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
-});
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
